5643c89407a16ac5dc4bff69b5ecc6195b143c33
ALISHA ROUT <routalisha331@gmail.com>
2025-12-25 21:10:57 +0530

    Add files via upload

---
+++ thisoops.java
@@ -0,0 +1,10 @@
+public class thisoops {
+    int x;
+    public thisoops(int x){
+this.x=x;
+    }
+    public static void main(String[] args){
+        thisoops myObj = new thisoops(34);
+        System.out.println("value of x =" + myObj.x);
+    }
+}
